58ec2dc7709ed92c229f6efb4668b88a09d615a0
Ryan <ryanburdick14@gmail.com>
2019-05-08 21:05:14 -0500

    working gauge chart

--- static/js/app.js
+++ static/js/app.js
@@ -6,8 +6,8 @@ function buildMetadata(sample) {
   (async function(){
     const url = `/metadata/${sample}`;
     const response = await d3.json(url);
-    const mData = response;
-    console.log(mData);
+    const data = response;
+    console.log(data);
       // Use d3 to select the panel with id of `#sample-metadata`
     const metaData = d3.select("#sample-metadata")
       // Use `.html("") to clear any existing metadata
@@ -15,7 +15,7 @@ function buildMetadata(sample) {
       // Use `Object.entries` to add each key and value pair to the panel
       // Hint: Inside the loop, you will need to use d3 to append new
       // tags for each key-value in the metadata.
-    Object.entries(mData).forEach(([key, value]) => {
+    Object.entries(data).forEach(([key, value]) => {
         console.log(`${key}: ${value}`);
         const row = metaData.append("ul");
         const cell = metaData.append("li");
@@ -24,6 +24,70 @@ function buildMetadata(sample) {
     });
       // BONUS: Build the Gauge Chart
       // buildGauge(data.WFREQ);
+
+
+  var level = data.WFREQ;
+
+  // Trig to calc meter point
+  var degrees = 180 - (180 * (level/9)),
+      radius = .5;
+  var radians = degrees * Math.PI / 180;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+      pathX = String(x),
+      space = ' ',
+      pathY = String(y),
+      pathEnd = ' Z';
+  var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+  var plotData = [{ type: 'scatter',
+    x: [0], y:[0],
+      marker: {size: 28, color:'850000'},
+      showlegend: false,
+      name: 'washes',
+      text: level,
+      hoverinfo: 'text+name'},
+    { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+    rotation: 90,
+    text: ['8-9', '7-8', '6-7', '5-6',
+              '4-5', '3-4', '2-3', '1-2', '0-1'],
+    textinfo: 'text',
+    textposition:'inside',
+    marker: {colors:['rgba(8, 50, 0, .5)', 'rgba(10, 80, 0, .5)', 'rgba(12, 95, 0, .5)', 
+                          'rgba(14, 110, 0, .5)','rgba(110, 154, 22, .5)',
+                          'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                          'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                          'rgba(255, 255, 255, 0)']},
+    labels: ['8-9', '7-8', '6-7', '5-6',
+    '4-5', '3-4', '2-3', '1-2', '0-1'],
+    hoverinfo: 'label',
+    hole: .5,
+    type: 'pie',
+    showlegend: false
+  }];
+
+  var layout = {
+    shapes:[{
+        type: 'path',
+        path: path,
+        fillcolor: '850000',
+        line: {
+          color: '850000'
+        }
+      }],
+    title: 'Belly Button Washing Frequency',
+    height: 500,
+    width: 500,
+    xaxis: {zeroline:false, showticklabels:false,
+              showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+              showgrid: false, range: [-1, 1]}
+  };
+
+Plotly.newPlot('gauge', plotData, layout);
   })()
 }
 
@@ -65,9 +129,9 @@ function buildCharts(sample) {
     }
   const pieData2 = [pieData]
   const layout2 = {
-      title: "Belly Button Pie",
-      height: 600,
-      width: 600
+      title: 'Belly Button Pie Chart',
+      height: 500,
+      width: 500
     }
   Plotly.newPlot("pie", pieData2, layout2)
 })()
